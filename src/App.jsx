14e8de179c768b18e1da2cb633c3bf835ea42583
FidanHuseynova <tu544sn3v@code.edu.az>
2023-12-04 13:44:32 +0400

    son =css qalib

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment,haddleInputToplama,haddleInputCix,haddleInputVur,haddleInputBol} from './counterSlice'
+import { decrement, increment, haddleInputToplama, haddleInputCix, haddleInputVur, haddleInputBol ,resetCount } from './counterSlice'
 
 export function Counter() {
   const [inp, setinp] = useState("")
   const count = useSelector((state) => state.buadlazim_deyil.value)
   const dispatch = useDispatch()
 
+  const handleButtonClick = (operation) => {
+    dispatch(operation(+inp));
+    setinp(""); 
+  };
+
   return (
     <div>
       <div>
+      <button onClick={() => dispatch(resetCount())}>C</button>
+       
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -23,11 +30,11 @@ export function Counter() {
         >
           Decrement
         </button>
-        <input type="text"  value={inp} onChange={(e) => setinp(e.target.value)}/>
-        <button  onClick={(e) => dispatch(haddleInputToplama(+inp))}>topla</button>
-        <button  onClick={(e) => dispatch(haddleInputCix(+inp))}>cix</button>
-        <button  onClick={(e) => dispatch(haddleInputVur(+inp))}>vur</button>
-        <button  onClick={(e) => dispatch(haddleInputBol(+inp))}>bol</button>
+        <input type="text" value={inp} onChange={(e) => setinp(e.target.value)} />
+        <button onClick={() => handleButtonClick(haddleInputToplama)}>topla</button>
+        <button onClick={() => handleButtonClick(haddleInputCix)}>cix</button>
+        <button onClick={() => handleButtonClick(haddleInputVur)}>vur</button>
+        <button onClick={() => handleButtonClick(haddleInputBol)}>bol</button>
       </div>
     </div>
   )
